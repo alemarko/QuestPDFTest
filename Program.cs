ef3ad9fe53aca992254b348eca076f39e726a60e
Aleksandar <aleksandar.markovic@orbico.com>
2022-06-01 13:29:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using QuestPDF.Drawing;
+using QuestPDF.Previewer;
+using QuestPDFTest;
+
+FontManager.RegisterFontType("Liberation Sans Narrow", File.OpenRead(@"C:\Projects\QuestPDFTest\LiberationSansNarrow-Regular.ttf"));
+FontManager.RegisterFontType("Liberation Sans Narrow", File.OpenRead(@"C:\Projects\QuestPDFTest\LiberationSansNarrow-Bold.ttf"));
+FontManager.RegisterFontType("Liberation Sans Narrow", File.OpenRead(@"C:\Projects\QuestPDFTest\LiberationSansNarrow-Italic.ttf"));
+FontManager.RegisterFontType("Liberation Sans Narrow", File.OpenRead(@"C:\Projects\QuestPDFTest\LiberationSansNarrow-BoldItalic.ttf"));
+
+var model = "This is some text to display";
+var document = new VerySimpleDocument(model);
+
+document.ShowInPreviewer();
---
+++ VerySimpleDocument.cs
@@ -0,0 +1,53 @@
+﻿using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace QuestPDFTest;
+
+public class VerySimpleDocument : IDocument
+{
+	public string Model { get; }
+
+	public VerySimpleDocument(string model)
+	{
+		Model = model;
+	}
+
+	public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+
+	public void Compose(IDocumentContainer container)
+	{
+		container.Page(page => page.Content().Element(ComposeContent));
+	}
+
+	void ComposeContent(IContainer container)
+	{
+		container.PaddingVertical(40).Grid(grid => 
+		{
+			grid.VerticalSpacing(4);
+			grid.HorizontalSpacing(25);
+			grid.AlignCenter();
+			grid.Columns(2);
+
+			grid.Item().Text("Text");
+			grid.Item().Text("Description");
+
+			grid.Item(2).LineHorizontal(1, Unit.Millimetre);
+			
+			grid.Item().Text(Model);
+			grid.Item().Text("no font set (default font)");
+
+			grid.Item().Text(Model).FontFamily("Liberation Sans Narrow");
+			grid.Item().Text(".FontFamily(\"Liberation Sans Narrow\")");
+
+			grid.Item().Text(Model).FontFamily("Liberation Sans Narrow").Bold();
+			grid.Item().Text(".FontFamily(\"Liberation Sans Narrow\").Bold()");
+
+			grid.Item().Text(Model).FontFamily("Liberation Sans Narrow").Italic();
+			grid.Item().Text(".FontFamily(\"Liberation Sans Narrow\").Italic()");
+
+			grid.Item().Text(Model).FontFamily("Liberation Sans Narrow").Bold().Italic();
+			grid.Item().Text(".FontFamily(\"Liberation Sans Narrow\").Bold().Italic()");
+		});
+	}
+}
